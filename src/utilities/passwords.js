9ddce1798a90aa726fce9740f763b95919409b34
shohelrrrana <shohelrrrana@gmail.com>
2021-09-22 23:15:41 +0600

    password genarator

---
+++ src/utilities/passwords.js
@@ -0,0 +1,24 @@
+//dependencies
+const crypto = require('crypto');
+
+function getRandomSalt() {
+    return crypto.randomBytes(8).toString('hex').slice(0, 16);
+}
+
+function mix(password, salt) {
+    return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+}
+
+function generate(password) {
+    let salt = getRandomSalt();
+    let hash = mix(password, salt);
+
+    return { hash, salt };
+}
+
+function validate(password, hash, salt) {
+    let newHash = mix(password, salt);
+    return hash === newHash;
+}
+
+module.exports = { validate, generate };
